f7cc0d72d88bc9141e4c567fa92d33f15419bbba
singh1984baljeet <baljeetsinghm00@gmail.com>
2024-11-18 10:23:45 -0700

    First commit

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ const PORT = 3000;
 
 app.get("/user/:id", (req, res)=>{
     const userId = req.params.id;
-    res.send('User id is${userId}');
+    res.send('User id is ${userId}');
 });
+
+app.get('/search', (req,res) =>{
+    const query = req.query.q;
+    res.send(`The query search term is ${query}`);
+})
 
 app.listen(PORT, () => {
     console.log('Server is running on http://localhost:${PORT}');
